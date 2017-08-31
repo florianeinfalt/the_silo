f5df87b0748e3e9743bbbb2433bcf3b6e18abde0
Florian Einfalt <florian.einfalt@me.com>
2017-08-31 16:46:03 +0100

    Add script shortcuts to documentation

--- the_silo/scripts/reload.py
+++ the_silo/scripts/reload.py
@@ -1,5 +1,9 @@
 def all_reads():
-    """Reload all read nodes in the compositing script"""
+    """
+    Reload all read nodes in the compositing script
+
+    Shortcut: ``Ctrl+Shift+R``
+    """
     import nuke
     for node in nuke.allNodes('Read'):
         node.knob('reload').execute()
