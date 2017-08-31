8deaf3defb800d6938c74c6b78f1cf9848292043
Florian Einfalt <florian.einfalt@me.com>
2017-08-31 14:23:08 +0100

    Simplify all_reads function.

--- the_silo/scripts/reload.py
+++ the_silo/scripts/reload.py
@@ -1,6 +1,5 @@
 def all_reads():
     """Reload all read nodes in the compositing script"""
     import nuke
-    for node in nuke.allNodes():
-        if node.Class() == 'Read':
-            node.knob('reload').execute()
+    for node in nuke.allNodes('Read'):
+        node.knob('reload').execute()
